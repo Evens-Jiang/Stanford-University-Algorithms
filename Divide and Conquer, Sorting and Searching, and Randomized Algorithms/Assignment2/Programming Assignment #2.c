fa2e99fe105c7c6601c6850a870edb0710686bc2
HuangLiPang <leo89214749@hotmail.com>
2017-03-31 10:49:19 +0800

    Assignment2 completed.

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment2/Programming Assignment #2.c
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment2/Programming Assignment #2.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 long long mergeSortRecursive(int arr[], int result[], int start, int end) {
 	if(start >= end)
 		return 0;
 	int start1 = start, end1 = ((end - start) >> 1 ) + start;
 	int start2 = end1 + 1, end2 = end;
-	int k = start;
+	int k = start, i = 0;
 	long long inversionCounter = 0;
 	inversionCounter += mergeSortRecursive(arr, result, start1, end1);
 	inversionCounter += mergeSortRecursive(arr, result, start2, end2);
@@ -17,13 +16,12 @@ long long mergeSortRecursive(int arr[], int result[], int start, int end) {
 		else{
 			result[k] = arr[start2];
 			start2++;
-			inversionCounter++;
+			for(i = 0; i < end1 - start1 + 1; i++)
+				inversionCounter += 1;
 		}
 		k++;
 //		result[k] = arr[start1] < arr[start2] ? arr[start1] : arr[start2];
 	}
-	if(start2 == end2)
-		inversionCounter = inversionCounter * (end1 - start1 + 1);
 	while(start1 <= end1){
 		result[k] = arr[start1];
 		k++; 
@@ -38,32 +36,24 @@ long long mergeSortRecursive(int arr[], int result[], int start, int end) {
 		arr[k] = result[k];
 	return inversionCounter;
 }
-
 long long mergeSort(int arr[], const int length) {
 	int result[length];
 	return mergeSortRecursive(arr, result, 0, length - 1);
 }
-
 int main() {
 	FILE *pFile, *wFile;
 	int inputArray[100000];
-	
 	pFile = fopen("inputIntegers.txt", "r");
 	wFile = fopen("sortResult.txt", "w");
-	
 	if(!pFile)
 		printf("Fail to open file\n");
 	else
 		printf("Open file successfully!\n");
-	
 	for(int i = 0; i < 100000; i++)
 		fscanf(pFile, "%d", &inputArray[i]);
-	
 	printf("Inversion count = %lld\n", mergeSort(inputArray, sizeof(inputArray) / sizeof(int)));
-
 	for(int i = 0; i < 100000; i++)
 		fprintf(wFile, "%d\n", inputArray[i]);
-	
 	fclose (pFile);
 	fclose (wFile);
    	return 0;
