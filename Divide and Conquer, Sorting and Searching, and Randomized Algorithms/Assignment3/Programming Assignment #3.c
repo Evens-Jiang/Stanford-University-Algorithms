185dfff4689b0256adc6c0471e56f26a9cba47cc
HuangLiPang <leo89214749@hotmail.com>
2017-04-06 12:09:57 +0800

    Quicksort completed

--- Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment3/Programming Assignment #3.c
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment3/Programming Assignment #3.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-void swap(int a, int b){
-  int tmp;
-  tmp = a;
-  a = b;
-  b = tmp;                                                                                             
+void swap(int *a, int *b){
+  	int tmp = *a;
+  	*a = *b;
+  	*b = tmp;                                                                                             
 }
 
 int partition(int array[], int left, int right, int pivotIndex){
 	int pivotValue = array[pivotIndex], storeIndex = left;
-	swap(array[pivotIndex], array[right]);
+	swap(&array[pivotIndex], &array[right]);
 	
-	for(int i = 0; i < right; i++)
+	for(int i = left; i < right; i++)
 		if(array[i] <= pivotValue){
-			swap(array[storeIndex], array[i]);
+			swap(&array[storeIndex], &array[i]);
 			storeIndex++;
 		}
-	swap(array[storeIndex],array[right]);
+	swap(&array[storeIndex], &array[right]);
 	return storeIndex;
 }
 
 void quickSort(int array[], int left, int right){
 	if(right > left){
 		int newPivotIndex = partition(array, left, right, left);
-		quickSort(array, left, newPivotIndex);
-		quickSort(array, newPivotIndex, right);
+		quickSort(array, left, newPivotIndex - 1);
+		quickSort(array, newPivotIndex + 1, right);
 	}
 }
 
@@ -40,10 +39,10 @@ void main(void){
 		printf("Open file successfully!\n");
 	for(i = 0; i < size; i++)
 		fscanf(inFile, "%d", &inputArray[i]);
-	fclose (inFile);
 	quickSort(inputArray, 0, size - 1);
-	for(i = 0; i < size; i++)
+	for(i = 0; i < size - 1; i++)
 		fprintf(wFile, "%d\n", inputArray[i]);
+	fprintf(wFile, "%d", inputArray[size - 1]);
+	fclose (inFile);
 	fclose (wFile);
-	return;
 }
