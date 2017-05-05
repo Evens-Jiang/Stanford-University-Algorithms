344d88dcca495802aa6ad1dbeaaefe23737344a8
HuangLiPang <leo89214749@hotmail.com>
2017-05-05 17:26:01 +0800

    Working on Dijkstra

--- 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
+++ 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
@@ -1,6 +1,21 @@
 /*
-Dijkstra's algorithm
-
+	Dijkstra's algorithm
+	function Dijkstra(Graph, source):
+		create vertex set Q
+		for each vertex v in Graph:             // Initialization
+			dist[v] ← INFINITY                  // Unknown distance from source to v
+			prev[v] ← UNDEFINED                 // Previous node in optimal path from source
+			add v to Q                          // All nodes initially in Q (unvisited nodes)
+		dist[source] ← 0                        // Distance from source to source
+		while Q is not empty:
+			u ← vertex in Q with min dist[u]    // Node with the least distance will be selected first
+			remove u from Q 
+			for each neighbor v of u:           // where v is still in Q.
+				alt ← dist[u] + length(u, v)
+				if alt < dist[v]:               // A shorter path to v has been found
+					dist[v] ← alt 
+					prev[v] ← u 
+		return dist[], prev[]
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -68,6 +83,10 @@ void makeAdjacencyList(FILE *File, graph_p graph){
 	}
 }
 
+void Dijkstra(graph_p graph, int source){
+	int vertexSet[SIZE + 1], distance[SIZE + 1], preVertex[SIZE + 1];
+}
+
 void main(){
 	FILE *inFile = fopen("dijkstraData.txt", "r");
 	
@@ -81,6 +100,7 @@ void main(){
 
 	makeAdjacencyList(inFile, graph);
 	displayGraph(graph);
+	destroyGraph(graph);
 	
 	fclose(inFile);
 }
