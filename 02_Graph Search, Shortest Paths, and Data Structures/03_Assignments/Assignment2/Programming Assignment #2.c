6b678bf565315f289d814e1747c9220aacd4090a
LEO-PC\Leo Huang <leo89214749@hotmail.com>
2017-05-07 18:34:27 +0800

    Assignment2 completed.

--- 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
+++ 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
@@ -1,4 +1,11 @@
 /*
+	Using min heap to build extract min queue
+	Heapify code is from wikipedia: https://zh.wikipedia.org/wiki/%E5%A0%86%E6%8E%92%E5%BA%8F
+
+	Graph adjacency list source code "graph.h" is from Varun Gupta's blog
+	http://simplestcodings.blogspot.tw/2013/09/graphs.html?view=sidebar
+*/
+/*
 	Dijkstra's algorithm
 	function Dijkstra(Graph, source):
 		create vertex set Q
@@ -17,6 +24,10 @@
 					prev[v] ← u 
 		return dist[], prev[]
 */
+/*
+	Vertex: {    7,  37,  59,  82,  99, 115, 133, 165, 188, 197 }
+	Answer: { 2599,2610,2947,2052,2367,2399,2029,2442,2505,3068 }
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -24,6 +35,148 @@
 
 #define SIZE 200
 typedef enum {FALSE = 0, TRUE} bool;
+
+typedef struct vertex_distance_queue{
+	int vertex, distance;
+}vertexDistanceQueue;
+
+int convertCharToInt(char*, int digit);
+void makeAdjacencyList(FILE *File, graph_p graph);
+void swap_set(vertexDistanceQueue* a, vertexDistanceQueue* b);
+void min_heapify(vertexDistanceQueue arr[], int start, int end);
+int extract_min(vertexDistanceQueue vertexSet[], int finished);
+void updateDistanceInQueue(vertexDistanceQueue vertexSet[], adjlist_p adjList, int finished, int distance, int* pre){
+	int length = SIZE - finished;
+	int i, j;
+	bool vertexFinished = FALSE;
+	adjlist_node_p nodePtr = adjList->head;
+	// printf("distance = %d\n", distance);
+	while(nodePtr != NULL){
+		for(i = 1; i <= length; i++){
+			if(vertexSet[i].vertex == nodePtr->vertex){
+				// printf("vertex = %d\n", vertexSet[i].vertex);
+				if((nodePtr->distance + distance) < vertexSet[i].distance){
+					// printf("vertexSet[%d].distance = %d\n", i, vertexSet[i].distance);
+					// printf("nodePtr->distance = %d\n", nodePtr->distance);
+					vertexSet[i].distance = nodePtr->distance + distance;
+					pre[vertexSet[i].vertex] = vertexSet[length + 1].vertex;
+				}
+				nodePtr = nodePtr->next;
+				break;
+			}
+			for(j = SIZE; j > length; j--)
+				if(vertexSet[j].vertex == nodePtr->vertex){
+					vertexFinished = TRUE;
+					nodePtr = nodePtr->next;
+					break;
+				}
+			if(vertexFinished){
+				vertexFinished = FALSE;
+				break; 
+			}
+		}
+	}
+}
+int* Dijkstra(graph_p graph, int source){
+	int *distance = malloc((SIZE + 1) * sizeof(int));
+	int *preVertex = malloc((SIZE + 1) * sizeof(int));
+	int i, j;
+	int finished = 0, vertexU, vertexV;
+	int tempDistance;
+	adjlist_p adjListPtr;
+	adjlist_node_p nodePtr;
+	vertexDistanceQueue vertexSet[SIZE + 1];
+	
+	for(i = 0; i <= SIZE; i++){
+		vertexSet[i].vertex = i;
+		vertexSet[i].distance = 1000000;
+		distance[i] = 1000000;
+		preVertex[i] = -1;
+	}
+	distance[source] = 0;
+	vertexSet[source].distance = 0;
+	swap_set(&vertexSet[1], &vertexSet[SIZE]);
+	finished++;
+	adjListPtr = &(graph->adjListArr[source]);
+	nodePtr = adjListPtr->head;
+	while(nodePtr != NULL){
+		vertexV = nodePtr->vertex;
+		tempDistance = nodePtr->distance + distance[source];
+		// printf("tempDistance = %d\n", tempDistance);
+		if(tempDistance < distance[vertexV]){
+			distance[vertexV] = tempDistance;
+			preVertex[vertexV] = source;
+		}
+		nodePtr = nodePtr->next;
+	}
+	updateDistanceInQueue(vertexSet, &(graph->adjListArr[source]), finished, distance[source], preVertex);
+
+
+	while(finished != SIZE - 1){
+		vertexU = extract_min(vertexSet, finished);
+		distance[vertexU] = vertexSet[SIZE - finished].distance;
+		finished++;
+		// printf("vertexU = %d\n", vertexU);
+		// printf("distance[vertexU] = %d\n", distance[vertexU]);
+		adjListPtr = &(graph->adjListArr[vertexU]);
+		nodePtr = adjListPtr->head;
+		while(nodePtr != NULL){
+			vertexV = nodePtr->vertex;
+			tempDistance = nodePtr->distance + distance[vertexU];
+			// printf("tempDistance = %d\n", tempDistance);
+			if(tempDistance < distance[vertexV]){
+				distance[vertexV] = tempDistance;
+				preVertex[vertexV] = vertexU;
+			}
+			nodePtr = nodePtr->next;
+		}
+		// printf("Walk all adjacenct node of U\n");
+
+		updateDistanceInQueue(vertexSet, &(graph->adjListArr[vertexU]), finished, distance[vertexU], preVertex);
+		// for(j = 1; j <= SIZE; j++){
+		// 	printf("%7d ", vertexSet[j].vertex);
+		// }
+		// printf("\n");
+		// for(j = 1; j <= SIZE; j++){
+		// 	printf("%7d ", vertexSet[j].distance);
+		// }
+		// printf("\nUpdate completed\n\n\n");
+	}
+	return distance;
+}
+void displayShortPath(int *vertexSet){
+	int i;
+	int array[] = {7,37,59,82,99,115,133,165,188,197};
+	printf("Answer: { ");
+	for(i = 0; i < sizeof(array) / sizeof(int) - 1; i++){
+		printf("%d,", vertexSet[array[i]]);
+	}
+	printf("%d }", vertexSet[array[i]]);
+	// for(i = 1; i <= SIZE; i++){
+	// 	printf("vertex %d = %d\n\n", i, vertexSet[i]);
+	// }
+}
+
+void main(){
+	FILE *inFile = fopen("dijkstraData.txt", "r");
+	
+	if(!inFile)
+		printf("Fail to open file\n\n");
+	else
+		printf("Open file successfully!\n\n");
+	
+	graph_p graph = createGraph(SIZE, DIRECTED);
+	printf("Graph created.\n\n");
+
+	makeAdjacencyList(inFile, graph);
+	// displayGraph(graph);
+
+	displayShortPath(Dijkstra(graph, 1));
+
+	destroyGraph(graph);
+	
+	fclose(inFile);
+}
 
 int convertCharToInt(char *c, int digit){
 	int number = 0, counter = digit, power = 0;
@@ -51,7 +204,6 @@ void makeAdjacencyList(FILE *File, graph_p graph){
 		if(v1Check){
 			if(c == 9){
 				v1 = convertCharToInt(temp, digit);
-//				printf("v1 = %d\n", v1);
 				digit = 0;
 				v1Check = FALSE;
 			}
@@ -61,7 +213,6 @@ void makeAdjacencyList(FILE *File, graph_p graph){
 			}
 		}
 		else if(c == 10){
-//			printf("\n");
 			v1Check = TRUE;
 		}
 		else{
@@ -72,7 +223,6 @@ void makeAdjacencyList(FILE *File, graph_p graph){
 			}
 			else if(c == 44){
 				v2 = convertCharToInt(temp, digit);
-//				printf("%4d ", v2);
 				digit = 0;
 			}
 			else{
@@ -83,24 +233,33 @@ void makeAdjacencyList(FILE *File, graph_p graph){
 	}
 }
 
-void Dijkstra(graph_p graph, int source){
-	int vertexSet[SIZE + 1], distance[SIZE + 1], preVertex[SIZE + 1];
+void swap_set(vertexDistanceQueue* a, vertexDistanceQueue* b) {
+	vertexDistanceQueue temp = *b;
+	*b = *a;
+	*a = temp;
 }
 
-void main(){
-	FILE *inFile = fopen("dijkstraData.txt", "r");
-	
-	if(!inFile)
-		printf("Fail to open file\n\n");
-	else
-		printf("Open file successfully!\n\n");
-	
-	graph_p graph = createGraph(SIZE, DIRECTED);
-	printf("Graph created.\n\n");
+void min_heapify(vertexDistanceQueue arr[], int start, int end) {
+	//建立父節點指標和子節點指標
+	int dad = start;
+	int son = dad * 2;
+	while (son <= end) { //若子節點指標在範圍內才做比較
+		if (son + 1 <= end && arr[son].distance > arr[son + 1].distance) //先比較兩個子節點大小，選擇最大的
+			son++;
+		if (arr[dad].distance < arr[son].distance) //如果父節點大於子節點代表調整完畢，直接跳出函數
+			return;
+		else { //否則交換父子內容再繼續子節點和孫節點比較
+			swap_set(&arr[dad], &arr[son]);
+			dad = son;
+			son = dad * 2;
+		}
+	}
+}
 
-	makeAdjacencyList(inFile, graph);
-	displayGraph(graph);
-	destroyGraph(graph);
-	
-	fclose(inFile);
+int extract_min(vertexDistanceQueue vertexSet[], int finished){
+	int i, length = SIZE - finished;
+	for(i = length / 2; i > 0; i--)
+		min_heapify(vertexSet, i, length);
+	swap_set(&vertexSet[1], &vertexSet[length]);
+	return vertexSet[length].vertex;
 }
