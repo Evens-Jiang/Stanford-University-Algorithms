46d788b8bb9f094cf03de16b9c64547fa0fc2b10
HuangLiPang <leo89214749@hotmail.com>
2017-05-08 14:28:48 +0800

    Add some comments.

--- 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
+++ 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment2/Programming Assignment #2.c
@@ -45,117 +45,9 @@ void makeAdjacencyList(FILE *File, graph_p graph);
 void swap_set(vertexDistanceQueue* a, vertexDistanceQueue* b);
 void min_heapify(vertexDistanceQueue arr[], int start, int end);
 int extract_min(vertexDistanceQueue vertexSet[], int finished);
-void updateDistanceInQueue(vertexDistanceQueue vertexSet[], adjlist_p adjList, int finished, int distance, int* pre){
-	int length = SIZE - finished;
-	int i, j;
-	bool vertexFinished = FALSE;
-	adjlist_node_p nodePtr = adjList->head;
-	// printf("distance = %d\n", distance);
-	while(nodePtr != NULL){
-		for(i = 1; i <= length; i++){
-			if(vertexSet[i].vertex == nodePtr->vertex){
-				// printf("vertex = %d\n", vertexSet[i].vertex);
-				if((nodePtr->distance + distance) < vertexSet[i].distance){
-					// printf("vertexSet[%d].distance = %d\n", i, vertexSet[i].distance);
-					// printf("nodePtr->distance = %d\n", nodePtr->distance);
-					vertexSet[i].distance = nodePtr->distance + distance;
-					pre[vertexSet[i].vertex] = vertexSet[length + 1].vertex;
-				}
-				nodePtr = nodePtr->next;
-				break;
-			}
-			for(j = SIZE; j > length; j--)
-				if(vertexSet[j].vertex == nodePtr->vertex){
-					vertexFinished = TRUE;
-					nodePtr = nodePtr->next;
-					break;
-				}
-			if(vertexFinished){
-				vertexFinished = FALSE;
-				break; 
-			}
-		}
-	}
-}
-int* Dijkstra(graph_p graph, int source){
-	int *distance = malloc((SIZE + 1) * sizeof(int));
-	int *preVertex = malloc((SIZE + 1) * sizeof(int));
-	int i, j;
-	int finished = 0, vertexU, vertexV;
-	int tempDistance;
-	adjlist_p adjListPtr;
-	adjlist_node_p nodePtr;
-	vertexDistanceQueue vertexSet[SIZE + 1];
-	
-	for(i = 0; i <= SIZE; i++){
-		vertexSet[i].vertex = i;
-		vertexSet[i].distance = 1000000;
-		distance[i] = 1000000;
-		preVertex[i] = -1;
-	}
-	distance[source] = 0;
-	vertexSet[source].distance = 0;
-	swap_set(&vertexSet[1], &vertexSet[SIZE]);
-	finished++;
-	adjListPtr = &(graph->adjListArr[source]);
-	nodePtr = adjListPtr->head;
-	while(nodePtr != NULL){
-		vertexV = nodePtr->vertex;
-		tempDistance = nodePtr->distance + distance[source];
-		// printf("tempDistance = %d\n", tempDistance);
-		if(tempDistance < distance[vertexV]){
-			distance[vertexV] = tempDistance;
-			preVertex[vertexV] = source;
-		}
-		nodePtr = nodePtr->next;
-	}
-	updateDistanceInQueue(vertexSet, &(graph->adjListArr[source]), finished, distance[source], preVertex);
-
-
-	while(finished != SIZE - 1){
-		vertexU = extract_min(vertexSet, finished);
-		distance[vertexU] = vertexSet[SIZE - finished].distance;
-		finished++;
-		// printf("vertexU = %d\n", vertexU);
-		// printf("distance[vertexU] = %d\n", distance[vertexU]);
-		adjListPtr = &(graph->adjListArr[vertexU]);
-		nodePtr = adjListPtr->head;
-		while(nodePtr != NULL){
-			vertexV = nodePtr->vertex;
-			tempDistance = nodePtr->distance + distance[vertexU];
-			// printf("tempDistance = %d\n", tempDistance);
-			if(tempDistance < distance[vertexV]){
-				distance[vertexV] = tempDistance;
-				preVertex[vertexV] = vertexU;
-			}
-			nodePtr = nodePtr->next;
-		}
-		// printf("Walk all adjacenct node of U\n");
-
-		updateDistanceInQueue(vertexSet, &(graph->adjListArr[vertexU]), finished, distance[vertexU], preVertex);
-		// for(j = 1; j <= SIZE; j++){
-		// 	printf("%7d ", vertexSet[j].vertex);
-		// }
-		// printf("\n");
-		// for(j = 1; j <= SIZE; j++){
-		// 	printf("%7d ", vertexSet[j].distance);
-		// }
-		// printf("\nUpdate completed\n\n\n");
-	}
-	return distance;
-}
-void displayShortPath(int *vertexSet){
-	int i;
-	int array[] = {7,37,59,82,99,115,133,165,188,197};
-	printf("Answer: { ");
-	for(i = 0; i < sizeof(array) / sizeof(int) - 1; i++){
-		printf("%d,", vertexSet[array[i]]);
-	}
-	printf("%d }", vertexSet[array[i]]);
-	// for(i = 1; i <= SIZE; i++){
-	// 	printf("vertex %d = %d\n\n", i, vertexSet[i]);
-	// }
-}
+void updateDistanceInQueue(vertexDistanceQueue vertexSet[], adjlist_p adjList, int finished, int distance, int* pre);
+int* Dijkstra(graph_p graph, int source);
+void displayShortestPath(int *vertexSet);
 
 void main(){
 	FILE *inFile = fopen("dijkstraData.txt", "r");
@@ -169,9 +61,9 @@ void main(){
 	printf("Graph created.\n\n");
 
 	makeAdjacencyList(inFile, graph);
-	// displayGraph(graph);
+//	displayGraph(graph);
 
-	displayShortPath(Dijkstra(graph, 1));
+	displayShortestPath(Dijkstra(graph, 1));
 
 	destroyGraph(graph);
 	
@@ -262,4 +154,134 @@ int extract_min(vertexDistanceQueue vertexSet[], int finished){
 		min_heapify(vertexSet, i, length);
 	swap_set(&vertexSet[1], &vertexSet[length]);
 	return vertexSet[length].vertex;
+}
+
+void updateDistanceInQueue(vertexDistanceQueue vertexSet[], adjlist_p adjList, int finished, int distance, int* preVertex){
+	/*	
+		SIZE = 5
+				 1 2 3 4 5
+		queue = |a|b|c|d|e|
+		finished = 2
+		length = 5 - 2 = 3
+		only need to search index 1 to 3
+	*/
+	int length = SIZE - finished;
+	int i, j;
+	bool vertexFinished = FALSE;
+	adjlist_node_p nodePtr = adjList->head;
+//	printf("distance = %d\n", distance);
+	
+	//Walk through the adjList
+	while(nodePtr != NULL){
+		for(i = 1; i <= length; i++){
+			if(vertexSet[i].vertex == nodePtr->vertex){
+//				printf("vertex = %d\n", vertexSet[i].vertex);
+				if((nodePtr->distance + distance) < vertexSet[i].distance){
+//					printf("vertexSet[%d].distance = %d\n", i, vertexSet[i].distance);
+//					printf("nodePtr->distance = %d\n", nodePtr->distance);
+					vertexSet[i].distance = nodePtr->distance + distance;
+					preVertex[vertexSet[i].vertex] = vertexSet[length + 1].vertex;
+				}
+				nodePtr = nodePtr->next;
+				break;
+			}
+			for(j = SIZE; j > length; j--)
+				if(vertexSet[j].vertex == nodePtr->vertex){
+					vertexFinished = TRUE;
+					nodePtr = nodePtr->next;
+					break;
+				}
+			if(vertexFinished){
+				vertexFinished = FALSE;
+				break; 
+			}
+		}
+	}
+}
+
+int* Dijkstra(graph_p graph, int source){
+	int *distance = malloc((SIZE + 1) * sizeof(int));	//Store the shortest path
+	int *preVertex = malloc((SIZE + 1) * sizeof(int));	//Store the preVertex
+	int i, j;
+	int finishedVertexNumber = 0, vertexU, vertexV;
+	int tempDistance;
+	adjlist_p adjListPtr;
+	adjlist_node_p nodePtr;
+	vertexDistanceQueue vertexSet[SIZE + 1];	//The record of present graph.
+	
+	for(i = 0; i <= SIZE; i++){
+		vertexSet[i].vertex = i;
+		vertexSet[i].distance = 1000000;
+		distance[i] = 1000000;
+		preVertex[i] = -1;
+	}
+	
+	//Initialize the source distance to 0.
+	distance[source] = 0;
+	vertexSet[source].distance = 0;
+	
+	//Switch the source to the last element in queue.
+	swap_set(&vertexSet[1], &vertexSet[SIZE]);
+	finishedVertexNumber++;
+	adjListPtr = &(graph->adjListArr[source]);
+	nodePtr = adjListPtr->head;
+	
+	//Initialize the shortest path in distance and preVertex.
+	while(nodePtr != NULL){
+		vertexV = nodePtr->vertex;
+		tempDistance = nodePtr->distance + distance[source];
+//		printf("tempDistance = %d\n", tempDistance);
+		if(tempDistance < distance[vertexV]){
+			distance[vertexV] = tempDistance;
+			preVertex[vertexV] = source;
+		}
+		nodePtr = nodePtr->next;
+	}
+	//Update the distance in queue.
+	updateDistanceInQueue(vertexSet, &(graph->adjListArr[source]), finishedVertexNumber, distance[source], preVertex);
+
+
+	while(finishedVertexNumber != SIZE - 1){
+		vertexU = extract_min(vertexSet, finishedVertexNumber);
+		distance[vertexU] = vertexSet[SIZE - finishedVertexNumber].distance;
+		finishedVertexNumber++;
+//		printf("vertexU = %d\n", vertexU);
+//		printf("distance[vertexU] = %d\n", distance[vertexU]);
+		adjListPtr = &(graph->adjListArr[vertexU]);
+		nodePtr = adjListPtr->head;
+		while(nodePtr != NULL){
+			vertexV = nodePtr->vertex;
+			tempDistance = nodePtr->distance + distance[vertexU];
+//			printf("tempDistance = %d\n", tempDistance);
+			if(tempDistance < distance[vertexV]){
+				distance[vertexV] = tempDistance;
+				preVertex[vertexV] = vertexU;
+			}
+			nodePtr = nodePtr->next;
+		}
+//		printf("Walk all adjacenct node of U\n");
+
+		updateDistanceInQueue(vertexSet, &(graph->adjListArr[vertexU]), finishedVertexNumber, distance[vertexU], preVertex);
+//		for(j = 1; j <= SIZE; j++)
+//			printf("%7d ", vertexSet[j].vertex);
+//		printf("\n");
+//		for(j = 1; j <= SIZE; j++)
+//		 	printf("%7d ", vertexSet[j].distance);
+//		printf("\nUpdate completed\n\n\n");
+	}
+	return distance;
+}
+
+void displayShortestPath(int *vertexSet){
+	int i;
+	int array[] = {7,37,59,82,99,115,133,165,188,197};
+	printf("Shortest path from source 1\n");
+	printf("Answer: { ");
+	for(i = 0; i < sizeof(array) / sizeof(int) - 1; i++){
+		printf("%d,", vertexSet[array[i]]);
+	}
+	printf("%d }", vertexSet[array[i]]);
+//	for(i = 1; i <= SIZE; i++){
+//		printf("vertex %d = %d\n\n", i, vertexSet[i]);
+//	}
 }
