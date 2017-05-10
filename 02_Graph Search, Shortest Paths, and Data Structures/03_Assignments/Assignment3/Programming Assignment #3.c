a3b53a0b78173f984fb4f9e1e9ede6c463622814
HuangLiPang <leo89214749@hotmail.com>
2017-05-10 14:48:03 +0800

    Add Duo E8400 running time test.

--- 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment3/Programming Assignment #3.c
+++ 02_Graph Search, Shortest Paths, and Data Structures/03_Assignments/Assignment3/Programming Assignment #3.c
@@ -10,10 +10,13 @@
 */
 /*	
 	The median maintenance is 1213.
-	CPU: i5-6500
-	executing 100 times.
-	Average running time is about 3.902 seconds. (Using min heap.)
-	Average running time is about 7.252 seconds. (Naive method, using heap sort sorting the array every time.)
+	Running time for executing 100 times.
+	1. CPU: i5-6500
+		Median maintenance:  3.902 seconds. (Using min heap.)
+		Naive method: 		 7.252 seconds. (Using heap sort sorting the array every time.)
+	2. CPU: Duo E8400
+		Median maintenance:  5.880 seconds. (Using min heap.)
+		Naive method: 		10.815 seconds. (Using heap sort sorting the array every time.)
 */
 #include <stdio.h>
 #include <time.h>
@@ -22,7 +25,7 @@
 
 void swap(int *a, int *b);
 void min_heapify(int array[], int start, int end);
-// void heap_sort(int arr[], int len);
+//void heap_sort(int arr[], int len);
 void bubbleUp(int array[], int son);
 void bubbleDown(int array[], int end);
 int extractMin(int array[], int end);
@@ -35,25 +38,25 @@ double median(void){
 	int i = 0;
 	int sum = 0;
 	
-	// if(!inFile)
-	// 	printf("Fail to open file\n");
-	// else
-	// 	printf("Open file successfully!\n");
+//	if(!inFile)
+//		printf("Fail to open file\n");
+//	else
+//		printf("Open file successfully!\n");
 	
 	while(fscanf(inFile, "%d", &array[i]) != EOF){
-			/*Median maintenance*/
+			/* Median maintenance */
 		bubbleUp(array, i);
 		sum += medianMaintenance(array, i);
-			/*Naive method*/
-		// heap_sort(array, i + 1);
-		// sum += array[i - i / 2];
+			/* Naive method */
+//		heap_sort(array, i + 1);
+//		sum += array[i - i / 2];
 		i++;
 	}
 	sum = sum % 10000;
-	// printf("Sum = %d\n", sum);
+//	printf("Sum = %d\n", sum);
 	fclose(inFile);
 	clock_t end = clock();
-	// printf("Running time = %.3f (sec)\n", (double)(end - begin) / CLOCKS_PER_SEC);
+//	printf("Running time = %.3f (sec)\n", (double)(end - begin) / CLOCKS_PER_SEC);
 	return (double)(end - begin) / CLOCKS_PER_SEC;
 }
 
