ebf454b376b453cfdfbd3aafda0fe96b2d0972ad
LEO-PC\Leo Huang <leo89214749@hotmail.com>
2017-06-08 23:51:04 +0800

    1. Added a test case of assignment 3-1 and 3-2. 2. Completed assignment 3-1 and 3-2.

--- 03_Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/03_Assignments/Assignment3/Programming Assignment #3-1.c
+++ 03_Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/03_Assignments/Assignment3/Programming Assignment #3-1.c
@@ -1,14 +1,38 @@
 /*
-    Huffman Coding Algorithm.
+    Huffman's Algorithm
+    (Given frequencies Pi as input)
+    If |∑| = 2 return
+
+           Node
+        0 /   \ 1
+         A     B
+
+    Let a, b belong to ∑ have the smallest frequencies.
+    Let ∑' = ∑ with a, b replaced by new symbol ab.
+    Define Pab = Pa + Pb.
+    Recursively compute T' (for the alphabet ∑')
+    Extend T' (with leaves ←→ ∑') to a tree T with leaves ←→ ∑ by splitting leaf ab into two leaves a & b.
+
+    Return T
+*/
+/*
+    Max = 19, min = 9.
+    runnig time = 0.090 (sec)
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "heap_sort.h"
+
+int findMax(huffman_node_p huffmanNode);
+int findMin(huffman_node_p huffmanNode);
 
 void main(void){
     clock_t begin = clock();
     FILE *inFile = fopen("huffman.txt", "r");
-    int numberOfSymbols, *weight, i = 0;
+    int numberOfSymbols, i = 0;
+    __int64 totalWeight = 0;
+    huffman_node_p huffmanNode;
     
     if(!inFile){
     	printf("Fail to open file\n");
@@ -20,17 +44,59 @@ void main(void){
     fscanf(inFile, "%d", &numberOfSymbols);
     printf("Number of symbols = %d\n", numberOfSymbols);
     
-    if((weight = malloc((numberOfSymbols + 1) * sizeof(int))) == NULL)
+    if((huffmanNode = malloc((numberOfSymbols) * sizeof(huffman_node_t))) == NULL)
         printf("Malloc weight failed.\n");
     
-    while(fscanf(inFile, "%d", &weight[i]) != EOF)
+    while(fscanf(inFile, "%lld", &huffmanNode[i].weight) != EOF){
+        totalWeight += huffmanNode[i].weight;
+        huffmanNode[i].key = i;
+        huffmanNode[i].left = NULL;
+        huffmanNode[i].right = NULL;
         i++;
-    printf("Completed reading weights.\n");
-    for(i = 1; i < 10; i++)
-        printf("%d\n", weight[i]);
-    
+    }
+
+    printf("totalWeight = %lld\n", totalWeight);
+    heap_sort(huffmanNode, numberOfSymbols);
+    i = numberOfSymbols - 1;
+    while(i != 0){
+        huffman_node_p newNode1 = malloc(sizeof(huffman_node_t)),  newNode2 = malloc(sizeof(huffman_node_t));
+        *newNode1 = huffmanNode[0];
+        *newNode2 = huffmanNode[1];
+        huffmanNode[0].weight = huffmanNode[0].weight + huffmanNode[1].weight;
+        huffmanNode[0].key = 0;
+        huffmanNode[0].left = newNode1;
+        huffmanNode[0].right = newNode2;
+        swap(&huffmanNode[i], &huffmanNode[1]);
+        heap_sort(huffmanNode, i);
+        i--;
+    }
+    printf("i = 0 weight = %lld\n", huffmanNode[0].weight);
+
+    printf("Max = %d\n", findMax(&huffmanNode[0]));
+    printf("Min = %d\n", findMin(&huffmanNode[0]));
+
     fclose(inFile);
     clock_t end = clock();
     printf("Running time = %.3f (sec)\n", (double)(end - begin) / CLOCKS_PER_SEC);
     return;
+}
+
+int findMax(huffman_node_p huffmanNode){
+    if(huffmanNode->left == NULL && huffmanNode->right == NULL)
+        return 0;
+    int left = findMax(huffmanNode->left), right = findMax(huffmanNode->right);
+    if(left > right)
+        return 1 + left;
+    else
+        return 1 + right;
+}
+
+int findMin(huffman_node_p huffmanNode){
+    if(huffmanNode->left == NULL && huffmanNode->right == NULL)
+        return 0;
+    int left = findMin(huffmanNode->left), right = findMin(huffmanNode->right);
+    if(left < right)
+        return 1 + left;
+    else
+        return 1 + right;
 }
--- 03_Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/03_Assignments/Assignment3/heap_sort.h
+++ 03_Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/03_Assignments/Assignment3/heap_sort.h
@@ -1,59 +1,42 @@
 #ifndef _HEAPSORT_H_
 #define _HEAPSORT_H_
 
-void swap(int *a, *b){
-    int temp = *a;
+typedef struct huffman_node{
+    int key;
+    __int64 weight;
+    struct huffman_node *left, *right;
+}huffman_node_t, *huffman_node_p;
+
+void swap(huffman_node_p a, huffman_node_p b){
+    huffman_node_t temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void bubbleUp(int *heap, int end){
-    int son = end, dad = (son / 2) % 2 ?  (son / 2) - 1 : (son / 2);
-    while(son > 0){
-        if(son < dad){
-            swap(&heap[son], &heap[dad]);
-            son = dad;
-            dad = (son / 2) % 2 ?  (son / 2) - 1 : (son / 2);
-        }
-        else
-            break;
-    }
-    return;
-}
-
-void bubbleDown(int *heap, int end){
-    int dad = 0, son = 1;
-    while(son <= end){
-        if(son + 1 < son && son + 1 <= end)
-            son = son + 1;
-        if(dad < son){
-            swap(&heap[son], &heap[dad]);
+void max_heapify(huffman_node_p huffmanNode, int start, int end) {
+    int dad = start;
+    int son = dad * 2 + 1;
+    while (son <= end) {
+        if (son + 1 <= end && huffmanNode[son].weight < huffmanNode[son + 1].weight)
+            son++;
+        if (huffmanNode[dad].weight > huffmanNode[son].weight)
+            return;
+        else {
+            swap(&huffmanNode[dad], &huffmanNode[son]);
             dad = son;
             son = dad * 2 + 1;
         }
-        else
-            break;
     }
-    return;
 }
 
-int insertHeap(int *heap, int end, int key){
-    end++;
-    heap[end] = key;
-    bubbleUp(heap, end);
-    return end;
-}
-
-int deleteHeap(int *heap, int end){
-    swap(&heap[0], &heap[end]);
-    end--;
-    bubbleDown(heap, end);
-    return end;
-}
-
-int extractMin(int *heap, int end, int *minKey){
-    *minKey = heap[0];
-    return deleteHeap(heap, end);
+void heap_sort(huffman_node_p huffmanNode, int len) {
+    int i;
+    for (i = len / 2 - 1; i >= 0; i--)
+        max_heapify(huffmanNode, i, len - 1);
+    for (i = len - 1; i > 0; i--){
+        swap(&huffmanNode[0], &huffmanNode[i]);
+        max_heapify(huffmanNode, 0, i - 1);
+    }
 }
 
 #endif
