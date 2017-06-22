6a9760a5823d59ca46e17b7f977b03e116cdfaa0
HuangLiPang <leo89214749@hotmail.com>
2017-06-22 15:36:02 +0800

    Assignment1 completed using Floyd-Warshall algorithm.

--- 04_Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/03_Assignments/Assignment1/bellman_ford.h
+++ 04_Shortest Paths Revisited, NP-Complete Problems and What To Do About Them/03_Assignments/Assignment1/bellman_ford.h
@@ -1,7 +1,8 @@
 /*
     Bellman Ford algorithm.
     Detecting negative cycle.
-    (https://www.dyclassroom.com/graph/detecting-negative-cycle-using-bellman-ford-algorithm)
+    The source code and description are from:
+    https://www.dyclassroom.com/graph/detecting-negative-cycle-using-bellman-ford-algorithm
     
     Step 1: We start by filling the distance array d[ ] with INFINITY and since vertex 0 is the source vertex so we will set d[0] = 0.
 
@@ -22,27 +23,26 @@
 #ifndef _BELLMANFORD_H_
 #define _BELLMANFORD_H_
 
-#define INFINITY 99999
+#define INFINITY 2147483647
 
 //struct for the edges of the graph
 typedef struct Edge {
-	int start;	//start vertex of the edge
-	int end;	//end vertex of the edge
-	int weight;	//weight of the edge (u,v)
+	int start;         //start vertex of the edge
+	int end;           //end vertex of the edge
+	int weight;        //weight of the edge (u,v)
 }edge_t, *edge_p;
 
 //Graph - it consists of edges
 typedef struct Graph {
-	int numberOfVertices	//total number of vertices in the graph
-	int numberOfEdges;	//total number of edges in the graph
-	edge_t *edges;	//array of edges
+	int numberOfVertices;
+	int numberOfEdges;
+	edge_t *edges;     //array of edges
 }graph_t, *graph_p;
 
 void errorExit(char* message) {
     printf("[Fatal Error]: %s \nExiting...\n", message);
     exit(1);
 }
-
 void addEdge(graph_p graph, int start, int end, int weight, int numberOfEdge){
     if(graph->numberOfEdges < numberOfEdge)
         errorExit("Edge number overflowed.");
@@ -51,7 +51,6 @@ void addEdge(graph_p graph, int start, int end, int weight, int numberOfEdge){
     graph->edges[numberOfEdge].weight = weight;
     return;
 }
-
 graph_p createGraph(int numberOfVertices, int numberOfEdges){
     graph_p newGraph = malloc(sizeof(graph_t));
     if(!newGraph)
@@ -62,14 +61,26 @@ graph_p createGraph(int numberOfVertices, int numberOfEdges){
     return newGraph;
 }
 
-bellmanFord(graph_p graph, int source){
+void killGraph(graph_p graph){
+    free(graph->edges);
+    free(graph);
+    return;
+}
+//void display(int arr[], int size) {
+//	int i;
+//	for(i = 1; i <= size; i ++) {
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+int bellmanFord(graph_p graph, int source){
     int numberOfVertices = graph->numberOfVertices, numberOfEdges = graph->numberOfEdges;
     int *distance, *predecessor;
-    int i;
+    int i, j, vertexU, vertexV, weight;
     
-    if(distance = malloc((numberOfVertices + 1) * sizeof(int)) == NULL)
+    if((distance = malloc((numberOfVertices + 1) * sizeof(int))) == NULL)
         errorExit("Malloc distance array failed.");
-    if(predecessor = malloc((numberOfVertices + 1) * sizeof(int)) == NULL)
+    if((predecessor = malloc((numberOfVertices + 1) * sizeof(int))) == NULL)
         errorExit("Malloc predecessor array failed.");
     /* Step1 & 2 */
     for(i = 0; i <= numberOfVertices; i++){
@@ -79,9 +90,35 @@ bellmanFord(graph_p graph, int source){
     distance[source] = 0;
     
     /* Step3 */
-    for(i = 0; i < numberOfVertices; i++)
-        
-    
+    for(i = 1; i < numberOfVertices; i++)
+        for(j = 0; j < numberOfEdges; j++){
+            vertexU = graph->edges[j].start;
+            vertexV = graph->edges[j].end;
+            weight = graph->edges[j].weight;
+            if(distance[vertexU] != INFINITY && distance[vertexV] > distance[vertexU] + weight){
+                distance[vertexV] = distance[vertexU] + weight;
+                predecessor[vertexV] = vertexU;
+            }
+        }
+    /* Step4 */
+    for(j = 0; j < numberOfEdges; j++){
+        vertexU = graph->edges[j].start;
+        vertexV = graph->edges[j].end;
+        weight = graph->edges[j].weight;
+        if(distance[vertexU] != INFINITY && distance[vertexV] > distance[vertexU] + weight){
+            printf("Negative weight cycle detected!\n\n");
+            free(distance);
+            free(predecessor);
+			return 0;
+        }
+    }
+    printf("No negative cycle detected!\n\n");
+    free(distance);
+    free(predecessor);
+//    printf("Distance array: ");
+//	display(distance, numberOfVertices);
+//	printf("Predecessor array: ");
+//	display(predecessor, numberOfVertices);
+    return 1;
 }
-
 #endif
